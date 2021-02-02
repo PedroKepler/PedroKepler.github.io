f89d1a92ecd816609d57de4b86f40d19afa1606a
PedroKepler <57542755+PedroKepler@users.noreply.github.com>
2021-02-01 22:41:09 -0300

    COMITANDO!

--- Desafio 01 e 02/04.js
+++ Desafio 01 e 02/04.js
@@ -1,24 +1,22 @@
-var nomes_array = ["Diego", "Gabriel", "Lucas"];
-
 function adicionar ()
 {
-	var elem = document.getElementById('apresent');
-    var valordoinput = document.getElementById('valor').value;
+	var elemento_quarto = document.getElementById('teste3');
+    var valordoinput = document.getElementById('text').value;
     
-	for (i of nomes_array) //Para percorrer todo o for ( java script cefet) 
+	for (i of  nomes_array) //Para percorrer todo o for ( java script cefet) 
 	{
-      var li = document.createElement('li');
-      var palavra = document.createTextNode(i);
+      var acrescentarli = document.createElement('li');
+      var texto = document.createTextNode(i);
 
-	  li.appendChild(palavra);
-	  elem.appendChild(li);
+	  acrescentarli.appendChild(texto);
+	  elemento_quarto.appendChild(acrescentarli);
 	}
 
-	var li = document.createElement('li');
-	var palavra = document.createTextNode(valordoinput);
+	var acrescentarli = document.createElement('li');
+	var texto = document.createTextNode(valordoinput);
 
-	li.appendChild(palavra);
-	elem.appendChild(li);
+	acrescentarli.appendChild(texto);
+	elemento_quarto.appendChild(acrescentarli);
 
     nomes_array.splice(0,3);
 }
